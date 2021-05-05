d7c7736257e398428677bf2aed23b3e98baf7875
pasquale <imparatopasq@gmail.com>
2021-05-05 15:54:08 +0200

    Implementazione visualizza e rimuovi da preferiti.

--- app/src/main/java/com/example/cinemates20/View/Fragment/PreferitiFragment.java
+++ app/src/main/java/com/example/cinemates20/View/Fragment/PreferitiFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -14,7 +13,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.cinemates20.Presenter.Fragment.PreferitiPresenter;
-import com.example.cinemates20.Presenter.Fragment.RicercaFilmPresenter;
 import com.example.cinemates20.R;
 
 public class PreferitiFragment extends Fragment {
