93d0795db284d3a7f21c739a91111d8b3bcd12a2
christian <christianmenna32@gmail.com>
2021-05-17 16:41:41 +0200

    Cambiati metodi DAO registrazione e login (JDBC GOODBYE)

--- app/src/main/java/com/example/cinemates20/DAO/UtenteDAO.java
+++ app/src/main/java/com/example/cinemates20/DAO/UtenteDAO.java
@@ -4,14 +4,27 @@ import android.util.Log;
 
 import com.example.cinemates20.Model.Utente;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class UtenteDAO {
 
+    private String url = "http://ec2-18-196-42-56.eu-central-1.compute.amazonaws.com";
     private Connection con;
 
     private boolean connect(){
@@ -22,7 +35,28 @@ public class UtenteDAO {
     }
 
     public int registerUser(String username, String pwd, String email){
-        boolean isCon = connect();
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", pwd));
+        params.add(new BasicNameValuePair("email", email));
+        HttpClient client = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url+"/registrazione.php");
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = client.execute(httpPost);
+            String responseString = EntityUtils.toString(response.getEntity());
+            JSONObject jsonObject = new JSONObject(responseString);
+            int check = jsonObject.getInt("response");
+            if(check != 0)
+                return 2;
+        } catch (Throwable e) {
+            e.printStackTrace();
+            Log.e("Error register user","Impossibile registrare utente");
+            return 1;
+        }
+        return 0;
+
+        /*boolean isCon = connect();
         if(isCon==false)
             return 2;
         String query = "INSERT INTO utente (username,password,email) VALUES (?,?,?)";
@@ -40,10 +74,34 @@ public class UtenteDAO {
         finally {
             closeConnection();
         }
-        return 0;
+        return 0;*/
     }
 
     public int login(String username, String password){
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
+        HttpClient client = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url+"/login.php");
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = client.execute(httpPost);
+            String responseString = EntityUtils.toString(response.getEntity());
+            JSONArray jsonArray = new JSONArray(responseString);
+            int n = jsonArray.length();
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                count = jsonObject.getInt("count");
+            }
+            if(count==0)
+                return 1;
+        } catch (Throwable e) {
+            Log.e("Error login user","Impossibile loggare utente");
+            return 2;
+        }
+        return 0;
+        /*
         boolean isCon = connect();
         if(isCon==false)
             return 3;
@@ -66,7 +124,7 @@ public class UtenteDAO {
             closeConnection();
         }
 
-        return 0;
+        return 0;*/
     }
 
     public Utente prelevaUtente(String username){
@@ -84,6 +142,7 @@ public class UtenteDAO {
             utente = new Utente(username,email);
             st.close();
         } catch (SQLException throwables) {
+            throwables.printStackTrace();
             Log.e("Error get user","Impossibile prelevare l'username");
             return null;
         }
@@ -94,7 +153,29 @@ public class UtenteDAO {
     }
 
     public int checkUser(String username){
-        boolean isCon = connect();
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("username", username));
+        HttpClient client = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url+"/checkUser.php");
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = client.execute(httpPost);
+            String responseString = EntityUtils.toString(response.getEntity());
+            JSONArray jsonArray = new JSONArray(responseString);
+            int n = jsonArray.length();
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                count = jsonObject.getInt("count");
+            }
+            if(count!=0)
+                return 1;
+        } catch (Throwable e) {
+            Log.e("Error login user","Impossibile loggare utente");
+            return 2;
+        }
+        return 0;
+        /*boolean isCon = connect();
         if(isCon==false)
             return 3;
         String query = "SELECT COUNT(*) AS count FROM utente WHERE username=?";
@@ -115,11 +196,33 @@ public class UtenteDAO {
         finally {
             closeConnection();
         }
-        return 0;
+        return 0;*/
     }
 
     public int checkEmail(String email){
-        boolean isCon = connect();
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("email", email));
+        HttpClient client = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url+"/checkEmail.php");
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = client.execute(httpPost);
+            String responseString = EntityUtils.toString(response.getEntity());
+            JSONArray jsonArray = new JSONArray(responseString);
+            int n = jsonArray.length();
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                count = jsonObject.getInt("count");
+            }
+            if(count!=0)
+                return 1;
+        } catch (Throwable e) {
+            Log.e("Error login user","Impossibile loggare utente");
+            return 2;
+        }
+        return 0;
+        /*boolean isCon = connect();
         if(isCon==false)
             return 3;
         String query = "SELECT COUNT(*) AS count FROM utente WHERE email=?";
@@ -140,7 +243,7 @@ public class UtenteDAO {
         finally {
             closeConnection();
         }
-        return 0;
+        return 0;*/
     }
 
     public ArrayList<String> prelevaUsernameAmici(String username){
--- app/src/main/java/com/example/cinemates20/View/Activity/MainActivity.java
+++ app/src/main/java/com/example/cinemates20/View/Activity/MainActivity.java
@@ -9,12 +9,10 @@ import com.example.cinemates20.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new MainPresenter(this);
     }
-
 }
